85e64d1d7b77f84c6a049ea0eed16fe1ab118b62
SergKramarev <kramarevsergey@yahoo.com>
2019-09-24 17:51:22 +0300

    visualization added

--- explosions/data_analysis.R
+++ explosions/data_analysis.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(lubridate)
-
+library(plotly)
 
 # reading data
 nuclear_explosions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-20/nuclear_explosions.csv")
@@ -8,3 +8,26 @@ nuclear_explosions <- readr::read_csv("https://raw.githubusercontent.com/rfordat
 nuclear_explosions <- nuclear_explosions %>%
     mutate(date_long = ymd(date_long),
            latutudelongitude = paste(as.character(latitude), as.character(longitude), sep = ":"))
+
+
+
+# plotly visualization
+p <- nuclear_explosions %>% plot_mapbox(lat = ~latitude, lon = ~longitude, size = ~magnitude_body, mode = 'scattermapbox', frame = ~country)
+
+
+
+#  Gvis visualization
+p <- gvisGeoChart(nuclear_explosions, locationvar = "latutudelongitude")
+
+# Leaflet visualization
+g <- leaflet(nuclear_explosions) %>% addTiles() %>% addMarkers(lng = ~longitude, lat = ~latitude, clusterOptions = markerClusterOptions())
+getColor <- function(nuclear_explosions) {
+    sapply(quakes$country, function(country) {
+    if(country == "USA") {
+    "green"
+    } else if(country == "USSR") {
+    "orange"
+    } else {
+    "red"
+    } })
+    }
